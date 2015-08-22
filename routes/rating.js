72b39aabcfa7b015c5aedc8ba27de418bb5b58a0
patrick <1437551414@qq.com>
2015-08-23 01:15:33 +0800

    for

---
+++ routes/rating.js
@@ -0,0 +1,131 @@
+var express = require('express');
+var router = express.Router();
+var utils=require('../utils');
+var db=require('../db/mongo_schema');
+var when=require('when');
+
+
+//db.Users.remove({name:'23'}).then(function(data){});
+    //db.Users.update({_id:'55d698f9593ebb4f45843b3e'},{$set:{name:'23'}}).then(function(data){
+    //});
+   // new db.Users({name:'25',password:'ff',wechat:{openid:'fsdfawef'}}).save(function(data){
+   //     if(data.errmsg){
+   //         console.log('error');
+   //     }else{
+   //         console.log('success');
+   //     }
+   // });
+
+var addPreBlocks=function(blocks){
+    var qlist=[];
+    for(var i in blocks){
+        qlist.push(
+                db.PreBlocks.findOne({
+                    keyword:blocks[i].keyword,
+                    block:blocks[i].result
+                }).exec()
+            );
+    }
+    return when.all(qlist).then(function(datas){
+        var map={};
+        for(var i in datas){
+            if(datas[i]){
+                map[datas[i].keyword+"_"+datas[i].block]=true;
+            }
+        }
+        var qlist=[];
+        for(var i in blocks){
+            if(!map[blocks[i].keyword+"_"+blocks[i].result]){
+                qlist.push(
+                  db.PreBlocks.create({
+                               keyword:blocks[i].result,
+                               block:blocks[i].keyword,
+                               state:0,
+                               creator:'55d6cc38bf966fff65ce2ad1'
+                  })
+                );
+                qlist.push(
+                  db.PreBlocks.create({
+                               keyword:blocks[i].keyword,
+                               block:blocks[i].result,
+                               state:0,
+                               creator:'55d6cc38bf966fff65ce2ad1'
+                  })
+                );
+            }
+        }
+        return when.all(qlist)
+    });
+}
+var addPreAliases=function(corrects){
+    var qlist=[];
+    for(var i in corrects){
+        qlist.push(
+                db.PreAliases.findOne({
+                    keyword:corrects[i].keyword,
+                    alias:corrects[i].result
+                }).exec()
+            );
+    }
+
+    return when.all(qlist).then(function(datas){
+        var map={};
+        for(var i in datas){
+            if(datas[i]){
+                map[datas[i].keyword+"_"+datas[i].alias]=true;
+            }
+        }
+        var qlist=[];
+        for(var i in corrects){
+            if(!map[corrects[i].keyword+"_"+corrects[i].result]){
+                qlist.push(
+                  db.PreAliases.create({
+                               keyword:corrects[i].result,
+                               alias:corrects[i].keyword,
+                               state:0,
+                               creator:'55d6cc38bf966fff65ce2ad1'
+                  })
+                );
+                qlist.push(
+                  db.PreAliases.create({
+                               keyword:corrects[i].keyword,
+                               alias:corrects[i].result,
+                               state:0,
+                               creator:'55d6cc38bf966fff65ce2ad1'
+                  })
+                );
+            }
+        }
+        return when.all(qlist);
+    });
+};
+
+router.post('/',function(req,res){
+    res.json({hehe:'f'});
+});
+
+function submit(datas){
+    var corrects=[];
+    var blocks=[];
+
+    for(var i in datas){
+        if(datas[i].correct){
+            corrects.push(datas[i]);
+        }else{
+            blocks.push(datas[i]);
+        }
+    }
+
+    var qlist=[];
+    if(corrects){
+        qlist.push(addPreAliases(corrects));
+    }
+    if(blocks){
+        qlist.push(addPreBlocks(blocks));
+    }
+    when.all(qlist).then(function(){
+        console.log('done');
+    });
+}
+
+module.exports=router;
---
+++ routes/search.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+var db=require('../db/mongo_schema');
+var nodegrass=require('nodegrass');
+var iconv=require('iconv-lite');
+var when=require('when');
+
+var options=require('../config').options;
+var search=require('./search_tools');
+var utils=require('../utils');
+
+//Search
+router.post('/', function(req,res) {
+	var keyword_origin=req.body.keyword;
+	if(!keyword_origin||keyword_origin.length==0){
+    	res.redirect('/');
+    	return;
+    }
+    keyword_origin=keyword_origin.substr(0,30);
+    keyword_origin=keyword_origin.trim();
+	var keyword=utils.before_translate_filter(keyword_origin);
+	var result={code:0,platform:utils.getPlatform(req),keyword:keyword_origin};
+	search.google_translate(keyword).then(function(keyword_translated){
+		keyword_translated=utils.after_translate_filter(keyword_translated);
+
+        var keywords={};
+        utils.after_translate_mix(keyword_origin,keywords);
+        utils.after_translate_mix(keyword_translated,keywords);
+        console.log(keywords);
+
+        qlist=[];
+        qlist_type=[];
+        if(req.body.type){
+            if(req.body.type=='scores'){
+                for(var i in keywords){
+                    qlist.push(search.google_imslp(i));
+                    qlist_type.push('scores');
+                }
+            }else if(req.body.type=='videos'){
+                for(var i in keywords){
+                    qlist.push(search.Youku(i));
+                    qlist_type.push('videos');
+                }
+            }else if(req.body.type=='audios'){
+                for(var i in keywords){
+                    qlist.push(search.QQMusic(i));
+                    qlist_type.push('audios');
+                }
+            }else{
+                res.json({code:-2,msg:'type error'});
+                return;
+            }
+        }else{
+            res.json({code:-1,msg:'type is missing'});
+            return;
+        }
+		when.all(qlist).then(function(datas){
+			result.videos=[];
+			result.scores=[];
+            result.audios=[];
+            var scores_ids={};
+            var videos_ids={};
+            var audios_ids={};
+			for(var i in datas){
+                if(qlist_type[i]=='scores'){
+                    for(var j in datas[i]){
+                        if(!scores_ids[datas[i][j].link]){
+                            scores_ids[datas[i][j].link]=true;
+                            result.scores.push(datas[i][j]);
+                        }
+                    }
+                }else if(qlist_type[i]=='videos'){
+                    for(var j in datas[i]){
+                        if(!videos_ids[datas[i][j].id]){
+                            videos_ids[datas[i][j].id]=true;
+                            result.videos.push(datas[i][j]);
+                        }
+                    }
+                }else if(qlist_type[i]=='audios'){
+                    for(var j in datas[i]){
+                        if(!audios_ids[datas[i][j].songid]){
+                            audios_ids[datas[i][j].songid]=true;
+                            result.audios.push(datas[i][j]);
+                        }
+                    }
+                }
+			}
+
+			res.json(result);
+		})
+	})
+});
+
+module.exports = router;
