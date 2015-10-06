6cc1f16d0adf551cf528a64c85a653256f4652d6
1437551414@qq.com <1437551414@qq.com>
2015-10-06 14:28:59 +0800

    mistake

--- routes/tools.js
+++ routes/tools.js
@@ -3,12 +3,33 @@ var when=require('when');
 var router = express.Router();
 var utils=require('../utils');
 var options=require('../config').options;
-var tools_helper=require('./tools_helper');
+///Only for router tools/google_search
+var google_search=function(keyword){
+        var url=options.google.api.search_url+'q='+utils.urlencode(keyword)+'&cx='+options.google.api.search_engine_id+'&key='+options.google.api.api_key+'&fields=items(title,link)';
+        return when.promise(function(resolve,reject){
+                utils.getHtml(url).then(function(data){
+                        data=JSON.parse(data);
+                        var result={code:0,result:[]};
+                        if(data.error){
+                                console.log(data);
+                                result.code=-1;
+                                result.msg='limited';
+                        }
+                        data=data.items;
+                        for(var i in data){
+                                result.result.push({
+                                        title:data[i].title,
+                                        link:data[i].link});
+                        }
+                        resolve(result);
+                });
+        });
+}
 
 router.get('/google_search',function(req,res){
         var keyword=req.query.keyword;
         var result={code:0};
-        tools_helper.google_search(keyword).then(function(data){
+        google_search(keyword).then(function(data){
                 if(data.code!=0){
                         result.code=data.code;
                         result.msg=data.msg;
