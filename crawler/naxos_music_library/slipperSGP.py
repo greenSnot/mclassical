206c11e17b16634f811f1b59fec9b700e9798229
Your Name <you@example.com>
2016-03-12 14:19:34 +0800

    slipperSGP

--- crawler/naxos_music_library/slipperSGP.py
+++ crawler/naxos_music_library/slipperSGP.py
@@ -6,62 +6,55 @@ from utils import *
 path='mclassical/crawler/naxos_music_library/resources_zips/'
 local_path='./resources_zips/'
 temp_path='~/'
-if __name__=='__main__':
-    @timeout(20)
-    def getRemoteFilesSum():
-        print 'getRemoteFilesSum'
-        res=int(os.popen('ssh root@mclassicalUSA "ls -l '+path+' |grep \'^-\'|wc -l"').read())
-        print res
-        return res
-    @timeout(20)
-    def getRemoteFiles():
-        print 'getRemoteFilesFiles'
-        return split(os.popen('ssh root@mclassicalUSA "ls '+path+'"').read().strip(),'\n')
-    @timeout(20)
-    def getRemoteMd5(filename):
-        print 'getRemoteMd5'
-        return split(os.popen('ssh root@mclassicalUSA "md5sum '+path+filename+'"').read(),' ')[0]
-    @timeout(200)
-    def scpRemote(filename):
-        print 'scpRemote'
-        print os.popen('scp root@mclassicalUSA:'+path+filename+' '+temp_path)
-    @timeout(20)
-    def rmRemote(filename):
-        print 'rmRemote'
-        print os.popen('ssh root@mclassicalUSA "rm '+path+filename+'"')
-    def setTimeoutRepeat(fun,**kwargs):
-        fetch=False
-        while not fetch:
-            try:
-                res=fun(**kwargs)
-                fetch=True
-            except Exception:
-                print 'TIMEOUT ERROR'
-        return res
 
+def getRemoteFilesSum(seconds):
+    print('getRemoteFilesSum')
+    cmd='ssh root@mclassicalUSA "ls -l '+path+' |grep \'^-\'|wc -l"'
+    return int(sub(cmd,seconds))
 
-    while True:
-        files_sum=setTimeoutRepeat(getRemoteFilesSum)
-        if files_sum>0:
-            files=setTimeoutRepeat(getRemoteFiles)
-            print files
-            for filename in files:
-                while int(os.popen('ls '+local_path+' |wc -l').read())>100:
-                    print 'waitting'
-                    time.sleep(600)
-                md5=setTimeoutRepeat(getRemoteMd5,filename=filename)
+def getRemoteFiles(seconds):
+    print('getRemoteFilesFiles')
+    cmd='ssh root@mclassicalUSA "ls '+path+'"'
+    return split(sub(cmd,seconds),'\n')[:-1]
 
-                fetch=False
-                while not fetch:
-                    print('downloading '+filename)
-                    setTimeoutRepeat(scpRemote,filename=filename)
-                    if md5==split(os.popen('md5sum '+temp_path+filename).read(),' ')[0]:
-                        setTimeoutRepeat(rmRemote,filename=filename)
-                        os.popen('mv '+temp_path+filename+' '+local_path)
-                        fetch=True
-                        print 'done '+filename
-                    else:
-                        print('error')
-                        os.popen('rm '+temp_path+filename)
-                        print 'md5 error retrying'
-        time.sleep(600)
+def getRemoteMd5(filename,seconds):
+    print('getRemoteMd5')
+    cmd='ssh root@mclassicalUSA "md5sum '+path+filename+'"'
+    return split(sub(cmd,seconds),' ')[0]
+
+def scpRemote(filename,seconds):
+    print('scpRemote')
+    cmd='scp root@mclassicalUSA:'+path+filename+' '+temp_path
+    return sub(cmd,seconds)
+
+def rmRemote(filename,seconds):
+    print('rmRemote')
+    cmd='ssh root@mclassicalUSA "rm '+path+filename+'"'
+    return sub(cmd,seconds)
+
+while True:
+    files_sum=setTimeoutRepeat(getRemoteFilesSum,seconds=5)
+    if files_sum>0:
+        print(files_sum)
+        files=setTimeoutRepeat(getRemoteFiles,seconds=5)
+        print(files)
+        for filename in files:
+            while int(os.popen('ls '+local_path+' |wc -l').read())>100:
+                print('waitting')
+                time.sleep(600)
+            md5=setTimeoutRepeat(getRemoteMd5,filename=filename,seconds=10)
+
+            fetch=False
+            while not fetch:
+                print('downloading '+filename)
+                setTimeoutRepeat(scpRemote,filename=filename,seconds=120)
+                if md5==split(os.popen('md5sum '+temp_path+filename).read(),' ')[0]:
+                    os.popen('mv '+temp_path+filename+' '+local_path)
+                    setTimeoutRepeat(rmRemote,filename=filename,seconds=10)
+                    fetch=True
+                    print('done '+filename)
+                else:
+                    print('error')
+                    os.popen('rm '+temp_path+filename)
+                    print('md5 error retrying')
+    time.sleep(600)
