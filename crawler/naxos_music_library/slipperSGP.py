04fb7997dca9b54f288dc9726c2e3ff798f24be2
patrick_冠仁 <patrick@codemao.cn>
2016-03-13 16:00:57 +0800

    up

--- crawler/naxos_music_library/slipperSGP.py
+++ crawler/naxos_music_library/slipperSGP.py
@@ -16,8 +16,15 @@ port='9000'
 def getRemoteFiles():
     print('getRemoteFilesFiles')
     files=getHtml(host+':'+port+'?ls=true',{},cache=False)
-    files=split(files,'\n')[:-1]
-    return files
+    files=split(files,'\n')[1:-2]
+    res=[]
+    for i in files:
+        t=split(i,' ')
+        if len(t)==3:
+            res.append({'size':int(t[1])/2,'name':t[2]})
+        else:
+            res.append({'size':int(t[0])/2,'name':t[1]})
+    return res
 
 def rmRemote(filename):
     print('rmRemote')
@@ -25,17 +32,17 @@ def rmRemote(filename):
 
 while True:
     files=getRemoteFiles()
-    print(files)
     if len(files)==0:
         print('len 0 waitting')
         time.sleep(60)
         continue
-    for filename in files:
+    for f in files:
+        filename=f['name']
         while int(os.popen('ls '+local_path+' |wc -l').read())>100:
             print('waitting')
             time.sleep(60)
         createDir(local_path)
         print quote(filename)
-        download(host+':'+port+'/'+remote_path+filename,local_path+filename,timeout=4000)
+        download(host+':'+port+'/'+remote_path+filename,local_path+filename,timeout=int(f['size']/200))
         rmRemote(filename)
         print('done '+filename)
