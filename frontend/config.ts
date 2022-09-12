79551be67b9594748b843d9fa90c18bed9bc1b0d
Your Name <you@example.com>
2022-09-13 06:56:13 +0800

    ci

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,9 +1,7 @@
 const default_cfg = require('../config/default').config;
-const local = require('../config/local');
 
 const conf = {
   ...default_cfg,
-  ...(local ? local.config : {}),
 };
 
 export default conf;
